304f62591f567c95e5cd896ec317ac8d7f8962b7
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-08-07 19:55:36 -0600

    Descriptions added

--- pages/projects.js
+++ pages/projects.js
@@ -58,9 +58,10 @@ export default function Projects() {
             />
           </a>
           <h3 className="text2xl pt-5 pb-3 font-lora font-medium md:text-center md:text-4xl dark:text-white">roomiehunt</h3>
-          <p className="text-md  leading-8 text-gray-800 font-poppins dark:text-slate-100">
-            Some Text
-          </p>
+            <p className="text-md  leading-8 text-gray-800 font-poppins dark:text-slate-100">
+              This site allows you to search for a <span className="text-sky-950 dark:text-white font-bold">roomie-hungry</span> house or apartment depending on preferences like location and pet-friendlyness. 
+              Or if you need more tenants for your home, you can <span className="text-sky-950 dark:text-white font-bold">upload</span> it and <span className="text-sky-950 dark:text-white font-bold">find</span> the best person to share with.  
+            </p>
         </div>
         <div className="basis-1/3 flex-1">
           <a href="https://oregonracing.club/" target="_blank">
@@ -75,7 +76,8 @@ export default function Projects() {
           </a>
           <h3 className="text2xl pt-5 pb-3 font-openSans font-medium md:text-center md:text-4xl dark:text-white">Oregon Racing</h3>
           <p className="text-md leading-8 text-gray-800 font-poppins dark:text-slate-100">
-            Some Text
+              Oregon Racing is a website created for the chain supplier <span className="text-sky-950 dark:text-white font-bold">OregonTool</span> and a campaign they got in order to give the best seller a 
+              free trip to <span className="text-sky-950 dark:text-white font-bold">Brasil</span> and tickets for the <span className="text-sky-950 dark:text-white font-bold">F1 Racing</span>.
           </p>
         </div>
         <div className="basis-1/3 flex-1">
@@ -94,10 +96,11 @@ export default function Projects() {
           </a>
           <h3 className="text2xl pt-5 pb-3 font-poppins font-medium md:text-center md:text-4xl dark:text-white">Text Encriptor</h3>
           <p className="text-md leading-8 text-gray-800 font-poppins dark:text-slate-100">
-            Some Text
+              This site allows you to type in any word or phrase you want, and <span className="text-sky-950 dark:text-white font-bold">encrypt</span> it via <span className="text-sky-950 dark:text-white font-bold">JavaScript</span>. 
+              It also allows to paste encrypted text and turn it back <span className="text-sky-950 dark:text-white font-bold">desencrypted</span>.
           </p>
         </div>
-        <div className="basis-1/3 flex-1 cursor-pointer">
+        <div className="basis-1/3 flex-1">
           <a href="https://santis1001.github.io/solar-waffle/" target="_blank">
             <Image
               src={solarwaffle}
@@ -106,14 +109,18 @@ export default function Projects() {
               width={"100%"}
               height={"100%"}
               layout="responsive"
+              cursor-pointer
             />
           </a>
           <h3 className="text2xl pt-5 pb-3 font-poppins font-medium md:text-center md:text-4xl dark:text-white">Solar Waffle</h3>
           <p className="text-md leading-8 text-gray-800 font-poppins dark:text-slate-100">
-            Some Text
+          This website allows users to navigate between the different planets of our solar system and view their information. 
+          The app offers user <span className="text-sky-950 dark:text-white font-bold">interaction and animation</span> for the planets.
+          Once on the planet information screen, the <span className="text-sky-950 dark:text-white font-bold">3D object</span> of the planet will enable interaction with the user, 
+          allowing them to move it around with the mouse.
           </p>
         </div>
-        <div className="basis-1/3 flex-1 cursor-pointer">
+        <div className="basis-1/3 flex-1">
           <a
             href="https://eduardopinedah.github.io/Code-Quiz-Game/"
             target="_blank"
@@ -125,11 +132,13 @@ export default function Projects() {
               width={"100%"}
               height={"100%"}
               layout="responsive"
+              cursor-pointer
             />
           </a>
           <h3 className="text2xl pt-5 pb-3 font-poppins font-medium md:text-center md:text-4xl dark:text-white">Coding Quiz</h3>
           <p className="text-md leading-8 text-gray-800 font-poppins dark:text-slate-100">
-            Some Text
+            On this site you can find <span className="text-sky-950 dark:text-white font-bold">code-related</span> quiz game with <span className="text-sky-950 dark:text-white font-bold">multiple answers</span>, 
+            with <span className="text-sky-950 dark:text-white font-bold">time limit</span> and a <span className="text-sky-950 dark:text-white font-bold">highscore</span> that keeps track of the scores.
           </p>
         </div>
       </div>
